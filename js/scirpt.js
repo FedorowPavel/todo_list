0c9457553602def582a1b7948ce36e8b5750d29c
FedorowPavel <pasha_fedorow@mail.ru>
2021-01-11 20:36:11 +0300

    added function to delete all checked tasks

--- js/scirpt.js
+++ js/scirpt.js
@@ -1,9 +1,16 @@
 import addTask from './task-operations/add-task.js';
+import deleteCheckedTasks from "./task-operations/delete-checked-tasks.js"
 
 //находим форму добавления
 const addForm = document.querySelector(".add-form");
 
 //вешаем обработчик события сабмит(отправку) на форму
 addForm.addEventListener("submit", addTask);
+
+//находим кнопку удаления отмеченых
+const deleteCheckedBtn = document.querySelector('.delete-checked-btn');
+//вешаем на нее обработчик событий
+deleteCheckedBtn.addEventListener('click', deleteCheckedTasks)
+
 
 
---
+++ js/task-operations/delete-checked-tasks.js
@@ -0,0 +1,10 @@
+function deleteCheckedTasks() {
+    const checkedTasks = document.querySelectorAll('li.checked');
+
+    checkedTasks.forEach((checkedTask) => {
+        checkedTask.remove();
+    })
+
+}
+
+export default deleteCheckedTasks;
