83c419376f0da971b300d82a208b4f08a474b31c
FedorowPavel <pasha_fedorow@mail.ru>
2021-01-15 13:47:41 +0200

    added funct. to delete chekced items from localstorage

--- js/task-operations/delete-checked-tasks.js
+++ js/task-operations/delete-checked-tasks.js
@@ -1,13 +1,20 @@
+import taskList from '../tasks.js';
+
 function deleteCheckedTasks() {
     const checkedTasks = document.querySelectorAll('li.checked');
     const deleteCheckedBtn = document.querySelector('.delete-checked-btn');
 
     checkedTasks.forEach((checkedTask) => {
         checkedTask.remove();
+
+        const taskId = parseInt(checkedTask.id);
+        taskList.delete(taskId);
     })
 
 
-    deleteCheckedBtn.setAttribute('disabled', "true")
+    deleteCheckedBtn.setAttribute('disabled', "true");
+
+    localStorage.setItem('tasks', JSON.stringify(taskList.tasks));
 
 }
 
